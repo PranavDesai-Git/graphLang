59a96d2f90df45ce780210bd904abcffab8cdac3
Pranav Desai <contact.pranavdesai@gmail.com>
2026-09-14 12:04:28 +0530

    Refactor plugins to use Node API via VMAPI

--- plugins/CoreMath.c
+++ plugins/CoreMath.c
@@ -4,130 +4,130 @@
 static VMAPI vm;
 
 Node *nativeAdd(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(l->data.literal + r->data.literal);
+    Node *res = vm.createLiteral(vm.getLiteral(l) + vm.getLiteral(r));
     vm.popRoot();
     return res;
 }
 
 Node *nativeSub(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(l->data.literal - r->data.literal);
+    Node *res = vm.createLiteral(vm.getLiteral(l) - vm.getLiteral(r));
     vm.popRoot();
     return res;
 }
 
 Node *nativeMul(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(l->data.literal * r->data.literal);
+    Node *res = vm.createLiteral(vm.getLiteral(l) * vm.getLiteral(r));
     vm.popRoot();
     return res;
 }
 
 Node *nativeDiv(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(l->data.literal / r->data.literal);
+    Node *res = vm.createLiteral(vm.getLiteral(l) / vm.getLiteral(r));
     vm.popRoot();
     return res;
 }
 
 Node *nativeLessThan(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(l->data.literal < r->data.literal ? 1 : 0);
+    Node *res = vm.createLiteral(vm.getLiteral(l) < vm.getLiteral(r) ? 1 : 0);
     vm.popRoot();
     return res;
 }
 
 Node *nativeGreaterThan(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(l->data.literal > r->data.literal ? 1 : 0);
+    Node *res = vm.createLiteral(vm.getLiteral(l) > vm.getLiteral(r) ? 1 : 0);
     vm.popRoot();
     return res;
 }
 
 Node *nativeEquals(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(l->data.literal == r->data.literal ? 1 : 0);
+    Node *res = vm.createLiteral(vm.getLiteral(l) == vm.getLiteral(r) ? 1 : 0);
     vm.popRoot();
     return res;
 }
 
 Node *nativeAnd(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
-    if (l->data.literal == 0) {
+    if (vm.getLiteral(l) == 0) {
         Node *res = vm.createLiteral(0);
         vm.popRoot();
         return res;
     }
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(r->data.literal != 0 ? 1 : 0);
+    Node *res = vm.createLiteral(vm.getLiteral(r) != 0 ? 1 : 0);
     vm.popRoot();
     return res;
 }
 
 Node *nativeOr(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
-    Node *right = args->right->left;
+    Node *left = vm.getLeft(args);
+    Node *right = vm.getLeft(vm.getRight(args));
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
-    if (l->data.literal != 0) {
+    if (vm.getLiteral(l) != 0) {
         Node *res = vm.createLiteral(1);
         vm.popRoot();
         return res;
     }
     Node *r = vm.evaluate(right, env);
-    Node *res = vm.createLiteral(r->data.literal != 0 ? 1 : 0);
+    Node *res = vm.createLiteral(vm.getLiteral(r) != 0 ? 1 : 0);
     vm.popRoot();
     return res;
 }
 
 Node *nativeNot(Node *args, struct LocalEnv *env) {
-    Node *left = args->left;
+    Node *left = vm.getLeft(args);
     Node *l = vm.evaluate(left, env);
     vm.pushRoot(l);
-    Node *res = vm.createLiteral(l->data.literal == 0 ? 1 : 0);
+    Node *res = vm.createLiteral(vm.getLiteral(l) == 0 ? 1 : 0);
     vm.popRoot();
     return res;
 }
 
 Node *nativeIf(Node *args, struct LocalEnv *env) {
-    Node *condition = args->left;
-    Node *trueBranch = args->right->left;
-    Node *falseBranch = args->right->right->left;
+    Node *condition = vm.getLeft(args);
+    Node *trueBranch = vm.getLeft(vm.getRight(args));
+    Node *falseBranch = vm.getLeft(vm.getRight(vm.getRight(args)));
 
     Node *cond = vm.evaluate(condition, env);
-    if (cond->data.literal == 1) {
+    if (vm.getLiteral(cond) == 1) {
         return vm.evaluate(trueBranch, env);
     } else {
         return vm.evaluate(falseBranch, env);
